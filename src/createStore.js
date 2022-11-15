e2f6874fc67fd80efdc860b8bdd6a1393dd8b290
Titus Muthomi <muthomititus+github@gmail.com>
2022-11-15 20:21:29 +0300

    Add createStoree

--- src/createStore.js
+++ src/createStore.js
@@ -1,28 +1,39 @@
-let state;
+function createStore(reducer) {
+	let state;
+
+	function dispatch(action) {
+		state = reducer(state, action);
+		render();
+	}
+
+	function getState() {
+		return state;
+	}
+
+	return { dispatch, getState };
+}
 
 function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
-    case "counter/increment":
-      return { count: state.count + 1 };
+	switch (action.type) {
+		case 'counter/increment':
+			return { count: state.count + 1 };
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
+let store = createStore(reducer);
+store.dispatch({ type: '@@INIT' });
 
 function render() {
-  let container = document.getElementById("container");
-  container.textContent = state.count;
+	let container = document.getElementById('container');
+	container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
-let button = document.getElementById("button");
+store.dispatch({ type: '@@INIT' });
+let button = document.getElementById('button');
 
-button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+button.addEventListener('click', function () {
+	store.dispatch({ type: 'counter/increment' });
 });
